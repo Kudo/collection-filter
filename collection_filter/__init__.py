7f84cf47924cc68adcc90a95a52c3dc4bf6b63a9
Kudo Chien <ckchien@gmail.com>
2015-10-28 14:45:49 +0800

    Bump for 0.5

--- collection_filter/__init__.py
+++ collection_filter/__init__.py
@@ -1,3 +1,3 @@
 from .collection_filter import collection_filter
 
-__version__ = '0.3'
+__version__ = '0.5'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import io
 
 setuptools.setup(
     name='collection-filter',
-    version='0.3',
+    version='0.5',
     description=(
         'A small list/dict DSL for field filter ' +
         'specific designed for RESTful API partial response.'
